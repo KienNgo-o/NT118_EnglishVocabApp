872ef8c1cdb1a42eb4ab5258e0c3ee73dd8195a2
PhamKhoi09 <phamkhoi090905@gmail.com>
2025-11-02 15:20:35 +0700

    fix bug logic from dialog return to sign in + adjust color (#9)

--- app/src/main/java/com/example/nt118_englishvocabapp/LoginActivity.java
+++ app/src/main/java/com/example/nt118_englishvocabapp/LoginActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 import com.example.nt118_englishvocabapp.R;
 
@@ -23,5 +24,19 @@ public class LoginActivity extends AppCompatActivity {
                     .replace(R.id.fragment_container_view_auth, new SignInFragment())
                     .commit();
         }
+    }
+
+    // Fix lỗi logic sau khi trở về Sign In từ CongaratulationDialogFragment
+    public void returnToSignInScreen() {
+        // Lấy FragmentManager
+        FragmentManager fm = getSupportFragmentManager();
+        // Xóa sạch toàn bộ back stack để loại bỏ các màn hình ForgotPassword
+        // Tham số thứ 2 là cờ (flag), POP_BACK_STACK_INCLUSIVE sẽ xóa tất cả các trạng thái đã lưu.
+        fm.popBackStack(null, FragmentManager.POP_BACK_STACK_INCLUSIVE);
+        // Sau khi đã xóa sạch, thay thế container bằng một SignInFragment mới.
+        // Vì back stack đã trống, đây sẽ là fragment duy nhất, và nó sẽ hoạt động đúng.
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.fragment_container_view_auth, new SignInFragment())
+                .commit();
     }
 }
